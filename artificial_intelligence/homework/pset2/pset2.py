5572498fda1cd36226d58cb978f4ad2f0c9f2c30
Basil R. Yap <basil.y.j.t@gmail.com>
2018-05-30 14:35:22 +0800

    Completed problem set 2 for AI

---
+++ artificial_intelligence/homework/pset2/pset2.py
@@ -0,0 +1,23 @@
+import learnThu8
+import numpy as np
+
+def gd(fxn, dfxn, x0, step_size=.01, max_iter=1000, eps=0.00001):
+	x = x0
+	ylist = []
+	xlist = []
+
+	for i in range(max_iter):
+		ylist.append(fxn(x))
+		xlist.append(x)
+		diff = step_size * dfxn(x)
+		x -= diff
+
+		if (np.absolute(diff) < eps).all():
+			break
+
+	return x, ylist, xlist
+
+learnThu8.gd = gd
+
+if __name__ == "__main__":
+	learnThu8.t6()
